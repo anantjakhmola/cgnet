7b36bbc17bc67b0ce6b11d1ed4382fdd5d3ea0c7
anant <34976779+anantjakhmola@users.noreply.github.com>
2021-09-21 11:36:18 +0530

    Update pods.go

--- kube/pods.go
+++ kube/pods.go
@@ -23,7 +23,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/api/core/v1"
 
 	log "github.com/inconshreveable/log15"
 )
